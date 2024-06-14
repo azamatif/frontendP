0cca4a7aa27cceb9b895bfcc6060edabf469c2d1
azamatif <159171451+azamatif@users.noreply.github.com>
2024-06-14 17:47:35 +0200

    Add files via upload
    rest of the pro

--- JS/script.js
+++ JS/script.js
@@ -15,37 +15,11 @@ $('.navbar-light .dropdown').on('hover', function () {
     $(this).on('find', '.dropdown-menu').first().stop(true, true).slideUp(50)
 });
 
+// carousel
 
- // carousel slider JS
-//  let currentSlide = 0;
-
-// function showSlide(index) {
-//     const slides = document.querySelectorAll('.carousel-item');
-//     if (index >= slides.length) {
-//         currentSlide = 0;
-//     } else if (index < 0) {
-//         currentSlide = slides.length - 1;
-//     } else {
-//         currentSlide = index;
-//     }
-//     const newTransformValue = -currentSlide * 100 + '%';
-//     document.querySelector('.carousel-inner').style.transform = `translateX(${newTransformValue})`;
-// }
-
-// function nextSlide() {
-//     showSlide(currentSlide + 1);
-// }
-
-// function prevSlide() {
-//     showSlide(currentSlide - 1);
-// }
-// $(document).ready(function(){
-//     $('.owl-carousel').owlCarousel({
-//         items: 1,
-//         loop: true,
-//         nav: true,
-//         autoplay: true,
-//         autoplayTimeout: 5000,
-//         navText: ['<i class="fa-solid fa-angles-left"></i>', '<i class="fa-solid fa-angles-right"></i>']
-//     });
-// });
+  document.addEventListener('DOMContentLoaded', function() {
+    var myCarousel = document.querySelector('#myCarousel');
+    var carousel = new bootstrap.Carousel(myCarousel, {
+      interval: 5000 
+    });
+  });
